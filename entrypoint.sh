05740a07d2e44fd319ec7112f1911651fee2fb4d
Samuel Fry <samuel.fry13@gmail.com>
2022-09-13 22:00:23 -0400

    fix entrypoint run cmd

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 [ -n "${PUID}" ] && usermod -o -u "${PUID}" node
 [ -n "${PGID}" ] && groupmod -o -g "${PGID}" node
 
-[ $INIT_DB ] && npm_config_yes=true runuser -u node -g node npx prisma db push --skip-generate
+[ $INIT_DB ] && npm_config_yes=true runuser -u node -g node -- npx prisma db push --skip-generate
 
-runuser -u node -g node "$@"
+runuser -u node -g node -- "$@"
 
