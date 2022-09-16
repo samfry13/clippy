8b2ed2ebe0e52049308763d11ce7c5ac3587d4e1
Samuel Fry <samuel.fry13@gmail.com>
2022-09-15 20:59:42 -0400

    add video chunk downloads

--- src/pages/api/v/[id].ts
+++ src/pages/api/v/[id].ts
@@ -14,12 +14,31 @@ const handler = (req: NextApiRequest, res: NextApiResponse) => {
     });
   }
 
-  const videoStream = fs.createReadStream(videoPath);
-  res.writeHead(200, {
-    'Content-Disposition': `attachment; filename=${id}.mp4`,
+  const { range } = req.headers;
+
+  if (!range) {
+    const videoStream = fs.createReadStream(videoPath);
+    res.writeHead(200, {
+      'Content-Disposition': `attachment; filename=${id}.mp4`,
+      'Content-Type': 'video/mp4',
+    });
+    return videoStream.pipe(res);
+  }
+
+  const videoSize = fs.statSync(videoPath).size;
+  const CHUNK_SIZE = 10 ** 6;
+  const start = Number(range.replace(/\D/g, ''));
+  const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+  const contentLength = end - start + 1;
+  const headers = {
+    'Content-Range': `bytes ${start}-${end}/${videoSize}`,
+    'Accept-Ranges': 'bytes',
+    'Content-Length': contentLength,
     'Content-Type': 'video/mp4',
-  });
-  return videoStream.pipe(res);
+  };
+  res.writeHead(206, headers);
+  const videoStream = fs.createReadStream(videoPath, { start, end });
+  videoStream.pipe(res);
 };
 
 export default handler;
