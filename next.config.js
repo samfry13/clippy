60868f6cbd2e931b0da8d9e470e5d08d6de971a5
Samuel Fry <samuel.fry13@gmail.com>
2023-09-17 11:22:17 -0400

    task: fix discord image not loading on sign in page

--- next.config.js
+++ next.config.js
@@ -3,7 +3,21 @@ const nextConfig = {
   headers() {
     return [
       {
-        source: "/:path*",
+        source: "/_next/:path*",
+        // These headers are to allow multi-threading ffmpeg
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+        ],
+      },
+      {
+        source: "/",
         // These headers are to allow multi-threading ffmpeg
         headers: [
           {
