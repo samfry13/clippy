afdb9217152a8136f6a9c0b2f40808b3ad2abf53
Samuel Fry <samuel.fry13@gmail.com>
2023-09-26 21:04:13 -0400

    feat: add custom error page

---
+++ src/app/(public)/auth-error/page.tsx
@@ -0,0 +1,106 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import { Button } from "~/components/ui/button";
+import { z } from "zod";
+import Link from "next/link";
+
+const ErrorMessage = {
+  Configuration: "Configuration",
+  AccessDenied: "AccessDenied",
+  Verification: "Verification",
+  Default: "Default",
+} as const;
+const ErrorMessagesSchema = z.nativeEnum(ErrorMessage);
+type ErrorMessage = z.infer<typeof ErrorMessagesSchema>;
+
+export default function Signin({
+  searchParams,
+}: {
+  searchParams?: { error: string | undefined };
+}) {
+  const errorParseResult = ErrorMessagesSchema.safeParse(searchParams?.error);
+
+  const error: ErrorMessage = errorParseResult.success
+    ? errorParseResult.data
+    : ErrorMessage.Default;
+
+  return (
+    <div className="max-w-xs mx-auto mt-20">
+      {error === ErrorMessage.Default && (
+        <Card>
+          <CardHeader>
+            <CardTitle>Error</CardTitle>
+          </CardHeader>
+
+          <CardContent className="flex flex-col">
+            <Button asChild>
+              <Link href="/">Home</Link>
+            </Button>
+          </CardContent>
+        </Card>
+      )}
+
+      {error === ErrorMessage.Configuration && (
+        <Card>
+          <CardHeader>
+            <CardTitle>Server Error</CardTitle>
+            <CardDescription>
+              There is a problem with the server configuration.
+            </CardDescription>
+            <CardDescription>
+              Check the server logs for more information.
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent className="flex flex-col">
+            <Button asChild>
+              <Link href="/">Home</Link>
+            </Button>
+          </CardContent>
+        </Card>
+      )}
+
+      {error === ErrorMessage.AccessDenied && (
+        <Card>
+          <CardHeader>
+            <CardTitle>Access Denied</CardTitle>
+            <CardDescription>
+              You do not have permission to sign in.
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent className="flex flex-col">
+            <Button asChild>
+              <Link href="/signin">Sign in</Link>
+            </Button>
+          </CardContent>
+        </Card>
+      )}
+
+      {error === ErrorMessage.Verification && (
+        <Card>
+          <CardHeader>
+            <CardTitle>Unable to sign in</CardTitle>
+            <CardDescription>
+              The sign in link is no longer valid.
+            </CardDescription>
+            <CardDescription>
+              It may have been used already or it may have expired.
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent className="flex flex-col">
+            <Button asChild>
+              <Link href="/signin">Sign in</Link>
+            </Button>
+          </CardContent>
+        </Card>
+      )}
+    </div>
+  );
+}
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -35,7 +35,7 @@ export const authOptions: AuthOptions = {
   pages: {
     signIn: "/signin",
     signOut: "/signout",
-    // error: '/auth/error', // Error code passed in query string as ?error=
+    error: "/auth-error", // Error code passed in query string as ?error=
     // verifyRequest: '/auth/verify-request', // (used for check email message)
     // newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
   },
